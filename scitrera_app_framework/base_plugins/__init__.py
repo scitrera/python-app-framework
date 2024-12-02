03419486b99557411e901d39bb4939c1e8a6916c
Drew Botwinick <drew@drewbotwinick.com>
2024-12-02 13:44:59 -0600

    chg: added register_package_plugins recursive option

--- scitrera_app_framework/base_plugins/__init__.py
+++ scitrera_app_framework/base_plugins/__init__.py
@@ -4,22 +4,23 @@ from .bg_exec import EXT_BACKGROUND_EXEC, JobExecutorEngine
 from .progress_tracker import EXT_PROGRESS_TRACKER, ProgressTracker
 
 
-def register_package_plugins(package: str, v: Variables = None, exclusions=()):
+def register_package_plugins(package: str, v: Variables = None, exclusions=(), recursive=False):
     """
     Register plugin classes found in the given package.
 
-    This function searches for all subclasses of the `Plugin` class within the specified package (non-recursively),
+    This function searches for all subclasses of the `Plugin` class within the specified package (non-recursively by default),
     and registers them. Plugins can be excluded from registration by specifying them in the `exclusions` parameter.
 
     :param package: The package or module in which to search for plugin classes.
     :param v: optional framework Variables instance to operate on; default system instance is used if not provided
     :param exclusions: An optional tuple of plugin classes that should be excluded from registration. Defaults to an empty tuple.
+    :param recursive: Whether to recursively search for packages that contains plugins. Defaults to False.
     """
     from ..api import Plugin
     from ..util.imports import find_types_in_modules
     from ..core.plugins import register_plugin
 
-    for plugin in find_types_in_modules(package, Plugin, recursive=False, exclude_base_type=True):
+    for plugin in find_types_in_modules(package, Plugin, recursive=recursive, exclude_base_type=True):
         if plugin in exclusions:
             continue
         register_plugin(plugin, v)
