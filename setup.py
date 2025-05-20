5d6f314cb1ffc499f181ad1b3e83d8406331b01c
Drew Botwinick <drew@drewbotwinick.com>
2025-05-20 16:17:05 -0500

    chore: version bump to 0.0.55

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setuptools.setup(
     name="scitrera-app-framework",
-    version="0.0.54",
+    version="0.0.55",
     author="Scitrera LLC",
     author_email="sales@scitrera.com",
     description="Common Application Framework Code and Utilities",
