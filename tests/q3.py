5d301c8b83cafb96dbd48fa223bfb6d3d499bed0
Drew Botwinick <drew@drewbotwinick.com>
2025-05-20 16:16:50 -0500

    fix: updated q3 "test" file to explicitly enable "base plugins" (previous version changed base plugins default to False)

--- tests/q3.py
+++ tests/q3.py
@@ -2,7 +2,7 @@ from scitrera_app_framework import (init_framework, get_extension, EXT_BACKGROUN
 from scitrera_app_framework.core import log_framework_variables
 
 if __name__ == '__main__':
-    init_framework('test-app-q3')
+    init_framework('test-app-q3', base_plugins=True)
 
     get_extension(EXT_BACKGROUND_EXEC).submit_job(print, 'bg exec job')
 
